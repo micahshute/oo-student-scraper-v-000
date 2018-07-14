8ac72396e86161003fbc8facf4379212a4f26096
Micah Shute <micah.shute@gmail.com>
2018-07-14 06:42:13 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,12 +6,12 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     html = Nokogiri::HTML(open(index_url))
+    student_info = html.css('div.student-card div.card-text-container')
     binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
